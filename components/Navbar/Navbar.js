dd805469531aca2b9bd5703d83f5b19dda8a254e
Nick <nikoloz.danelia@redberry.ge>
2023-12-12 01:23:41 +0400

    parallax, scroll indicator, navbar hover

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import { motion, useScroll, useSpring } from "framer-motion";
+export const Navbar = () => {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
+  return (
+    <>
+      <motion.div className="progress-bar" style={{ scaleX }} />
+      <motion.nav className="flex items-center justify-end py-6 px-16 fixed w-full top-0 left-0">
+        <ul className="flex text-white gap-8 items-center text-2xl">
+          <li className="underline-hover cursor-pointer">Experience</li>
+          <li className="cursor-pointer underline-hover">Skills</li>
+          <li className="cursor-pointer underline-hover">Contact</li>
+        </ul>
+      </motion.nav>
+    </>
+  );
+};
+
+export default Navbar;
