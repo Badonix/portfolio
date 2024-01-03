e86bd82a8f5665e504ab0afe6f9ebeb04fa0064f
Nick <nikdanelia@gmail.com>
2024-01-03 23:53:34 +0400

    style: chart animation

--- components/ThirdSection/ThirdSection.js
+++ components/ThirdSection/ThirdSection.js
@@ -1,11 +1,29 @@
+import { useEffect } from "react";
 import { useThirdSection } from "./useThirdSection";
+import { animate, inView } from "framer-motion";
 
 export const ThirdSection = () => {
+  useEffect(() => {
+    inView("#in-view2", (info) => {
+      const chartAnimation = animate(
+        ".chart",
+        { y: [200, 0], scale: [0, 1] },
+        { type: "spring", stiffness: 200, delay: 0.5, duration: 1.5 }
+      );
+
+      return () => {
+        chartAnimation.stop();
+      };
+    });
+  });
   const { options, data, Bar } = useThirdSection();
   return (
     <section className="h-screen w-screen absolute top-200">
-      <div className="w-full md:p-56 p-4 h-full flex items-center justify-center">
-        <div className="relative w-full h-full flex items-center justify-center">
+      <div
+        id="in-view2"
+        className="w-full md:p-56 p-4 h-full flex items-center justify-center"
+      >
+        <div className="chart relative w-full h-full flex items-center justify-center">
           <Bar options={options} data={data} />
         </div>
       </div>
