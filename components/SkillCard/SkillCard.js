00da481fd6f16d0ea0966742af56ad63e0424a52
Nick <nikdanelia@gmail.com>
2024-01-24 15:04:45 +0400

    fixed card titles (horrible fix, it might f up whole project)

--- components/SkillCard/SkillCard.js
+++ components/SkillCard/SkillCard.js
@@ -1,8 +1,8 @@
 export const SkillCard = ({ imageSource, title }) => {
   return (
     <div className="bg-white h-card rounded-xl relative w-full flex items-center justify-center flex-col">
-      <div className="absolute x-1/2 -translate-x-1/2 top-4">
-        <h1 className="text-center absolute x-1/2 -translate-x-1/2 top-4">
+      <div className="absolute card-thing x-1/2 -translate-x-1/2 top-4">
+        <h1 className="text-black card-thing text-center absolute x-1/2 -translate-x-1/2 top-4">
           {title}
         </h1>
       </div>
